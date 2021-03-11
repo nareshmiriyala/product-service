3ddca2c121df87ecaff2361cdb98ec5785bd2332
Naresh Miriyala <naresh.miriyala@redenergy.com.au>
2021-03-11 12:20:11 +1100

    Adding initial code

---
+++ src/main/groovy/au/com/company/productservice/ProductServiceApplication.groovy
@@ -0,0 +1,13 @@
+package au.com.company.productservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ProductServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication, args)
+	}
+
+}
---
+++ src/test/groovy/au/com/company/productservice/ProductServiceApplicationTests.groovy
@@ -0,0 +1,13 @@
+package au.com.company.productservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
